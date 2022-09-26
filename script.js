ba98e7385ce5dfe328087acaa8132fdffa6aa41b
tomifuchi <tomifuchi@yahoo.com>
2022-09-27 00:13:22 +0700

    Autoplay added

--- script.js
+++ script.js
@@ -20,25 +20,34 @@ expand_list_mb_button.addEventListener('click', () => {
     mb_button.dispatchEvent(new Event('click'));
 });
 
+function activeCarousel() {
+  //Image carousel
+  const offset = this.dataset.carouselButton === "next" ? 1 : -1;
+  scrollCarousel(offset);
+}
+
+function scrollCarousel(offset) {
+  const slides = document.querySelector('[data-carousel]').querySelectorAll('[data-carousel-slide]');
+  //Detail carousel
+  const details = document.querySelector('[data-carousel-detail]').querySelectorAll('[data-carousel-detail-item]');
+
+  const activeSlide = [...slides].indexOf(document.querySelector('[data-active-img]'));
+  //The [...<something>] turns this into an array
+  let newIndex = activeSlide + offset;
+
+  if(newIndex < 0) newIndex = slides.length - 1;
+  if(newIndex > slides.length - 1) newIndex = 0;
+
+  slides[newIndex].dataset.activeImg = true;
+  details[newIndex].dataset.activeDetail = true;
+  delete slides[activeSlide].dataset.activeImg;
+  delete details[activeSlide].dataset.activeDetail;
+}
+
 const carousel_buttons = document.querySelectorAll('[data-carousel-button]');
 carousel_buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        //Image carousel
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-        const slides = document.querySelector('[data-carousel]').querySelectorAll('[data-carousel-slide]');
-        //Detail carousel
-        const details = document.querySelector('[data-carousel-detail]').querySelectorAll('[data-carousel-detail-item]');
-
-        const activeSlide = [...slides].indexOf(document.querySelector('[data-active-img]'));
-        //The [...<something>] turns this into an array
-        let newIndex = activeSlide + offset;
-
-        if(newIndex < 0) newIndex = slides.length - 1;
-        if(newIndex > slides.length - 1) newIndex = 0;
-
-        slides[newIndex].dataset.activeImg = true;
-        details[newIndex].dataset.activeDetail = true;
-        delete slides[activeSlide].dataset.activeImg;
-        delete details[activeSlide].dataset.activeDetail;
-    });
+    button.addEventListener('click', activeCarousel);
 });
+
+//Auto play, loops forward in 60s or a minute
+setInterval(function () {scrollCarousel(1)}, 60000);
